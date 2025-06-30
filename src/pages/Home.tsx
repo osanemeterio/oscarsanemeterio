a50c8ef32bf1baabd32529488e847c024f83d4fc
osanemeterio <osanemeterio@gmail.com>
2025-06-30 15:20:23 +0200

    Create Home.tsx

---
+++ src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div className="min-h-screen flex items-center justify-center p-10">
+      <h1 className="text-4xl font-bold text-[#5a5b7f]">Página principal</h1>
+      <p className="mt-4 text-lg text-gray-700">Aquí irá tu landing completa.</p>
+    </div>
+  )
+}
+
+export default Home
